162d7d92063d9d38c0cb3137c2086e1a0fa36c59
devon\n.gaharwar <n.gaharwar@devon.nl>
2019-01-06 17:00:05 +0530

    update changes in HTML Code

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from flask_wtf import FlaskForm,Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -7,3 +7,20 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+class clientRegisterForm(FlaskForm):
+    client_name = StringField('ClientName', validators=[DataRequired()])
+    client_uri = StringField('ClientURI', validators=[DataRequired()])
+    scope = SelectField('Allowed Scopes', choices=[('read','Read'),('write','Write')], validators=[DataRequired()])
+    redirect_uri = StringField('RedirectURIs',validators=[DataRequired()])
+    grant_type = SelectField('Allowed Grant Types', choices=[('authorization_code','authorization_code'),('token','token'),('password','password'),
+                                                         ('client_credentials','client_credentials')], validators=[DataRequired()])
+    token_endpoint_auth_method = SelectField('Token Endpoint Auth Method', choices=[('client_secret_basic','client_secret_basic'),
+                                                        ('client_secret_post','client_secret_post'),('none','None')])
+    submit = SubmitField('Register')
+
+
+class userSignUpForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
-from .forms import LoginForm
+from app import app, db
+from .forms import LoginForm, clientRegisterForm, userSignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
 from .Models.User import User
+from .Models.Client import Client
 from werkzeug.urls import url_parse
+from werkzeug.security import gen_salt
 import flask_login
 from .oauth2 import authorization, config_oauth
 from authlib.specs.rfc6749 import OAuth2Error
@@ -52,6 +54,7 @@ def logout():
     return redirect(url_for('login'))
 
 @app.route('/oauth/authorize', methods=['GET','POST'])
+@login_required
 def authorize():
     config_oauth(app)
     user = current_user
@@ -84,5 +87,70 @@ def issue_token():
         print(state)
         grant_type = "authorization_code"
         redirect_uri = "http://127.0.0.1:5000/home"'''
+    #print(request.headers)
+    #print(request.data)
     return authorization.create_token_response()
     #return "Oauth token route"
+
+@app.route('/client_register', methods=['GET','POST'])
+@login_required
+def client_register():
+  user = current_user
+  if not user:
+      return redirect('/')
+
+  registerForm = clientRegisterForm()
+
+  print('Function reached')
+  if registerForm.validate_on_submit():
+      request_content=request.form.to_dict(flat=True)
+      print(*request_content)
+      del(request_content['csrf_token'])
+      del(request_content['submit'])
+      print(*request_content)
+
+      #client = Client(**request.form.to_dict(flat=True))
+      client = Client(**request_content)
+      client.user_id = user.id
+      client.client_id = gen_salt(24)
+      if client.token_endpoint_auth_method == 'none':
+        client.client_secret = ''
+      else:
+        client.client_secret = gen_salt(48)
+
+      if 'grant_type' in request_content and request_content['grant_type'] == "authorization_code":
+          client.response_type = "code"
+      elif 'grant_type' in request_content and request_content['grant_type'] == "token":
+          client.response_type = "token"
+      print(client.grant_type)
+      db.session.add(client)
+      db.session.commit()
+      flash("Client Registered Successfully")
+      #return redirect('/')
+  else:
+      print(registerForm.errors)
+
+
+  return render_template('createClient.html', title='Client Registeration', form=registerForm)
+
+
+@app.route('/sign-up',methods=['GET','POST'])
+def userSignUp():
+
+    signUpForm = userSignUpForm()
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User(username=username, password=password)
+
+        db.session.add(user)
+        db.session.commit()
+        flash("User Registered Successfully")
+      #return redirect('/')
+    else:
+       print(signUpForm.errors)
+
+
+    return render_template('userSignUp.html',title='Sign Up',form=signUpForm)
